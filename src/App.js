c2b32e9b3480d5c1c1e377ccb7620f869a5fbeb1
camilomontoyau <camilomontoya2@gmail.com>
2020-11-06 16:10:35 -0500

    puntos a realizar

--- src/App.js
+++ src/App.js
@@ -20,6 +20,8 @@ function ListStarWars() {
       <h1>Characters Star Wars</h1>
       {/* componente reusable por cada personaje nombre, altura, masa, color cabello, nombre del planeta */}
       {/* API en express, que permita hacer CRUD de cada persona */}
+      {/* debo poder crear, editar, eliminar y listar personajes desde mi propia API */}
+      {/* readme debe tener los pasos para correr el proyecto */}
       {characters.map((character, index) => (
         <li key={index}>{character.name}</li>
       ))}
