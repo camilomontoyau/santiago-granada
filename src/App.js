659bab7abae61b6dce626ca249b71acf5d9b1256
camilomontoyau <camilomontoya2@gmail.com>
2020-11-06 08:00:00 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
+import Axios from "axios";
 
 function App() {
+  const [personajes, setPersonajes] = useState({});
+  useEffect(() => {
+    Axios.get("https://swapi.dev/api/people/")
+      .then((response) => {
+        console.log(response);
+        setPersonajes(response.data.results);
+      })
+      .then((error) => {
+        console.log(error);
+      });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {personajes.map((personaje) => {
+        return personaje.name;
+      })}
     </div>
   );
 }
